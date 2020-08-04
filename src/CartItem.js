2a7eecbade5c7d5112c2752ccae8fff75052d58a
Deepak Jena <jenadeepak724@gmail.com>
2020-08-05 02:32:00 +0530

    added qyt plus and minus

--- src/CartItem.js
+++ src/CartItem.js
@@ -10,6 +10,33 @@ class CartItem  extends React.Component{
             qty: 1,
             img : ''
         }
+        // this.increaseQuantity = this.increaseQuantity.bind(this);
+    }
+    increaseQuantity = () =>{
+        // set state 1
+        // this.setState({
+        //     qty : this.state.qty+1
+        // });
+
+        // setstate 2
+        this.setState((prevState)=>{
+            return{
+                qty : prevState.qty + 1
+            }
+        });
+    }
+    decreaseQuantity = ()=>{
+        this.setState((prevState)=>{
+            if(prevState.qty==1){
+                return;
+            }
+            else{
+                return{
+                    qty : prevState.qty - 1
+                }
+            }
+        });
+
     }
     render() {
         const {title,price, qty, img} = this.state;
@@ -23,8 +50,8 @@ class CartItem  extends React.Component{
                     <div style={{color:'gray'}}>Rs {price}</div>
                     <div style={{color:'gray'}}>Qty: {qty}</div>
                     <div className="cart-item-actions">
-                        <img alt="increase" className="action-icons" src="https://image.flaticon.com/icons/svg/929/929409.svg"/>
-                        <img alt="decrease" className="action-icons" src="https://image.flaticon.com/icons/svg/561/561240.svg"/>
+                        <img alt="increase" className="action-icons" src="https://image.flaticon.com/icons/svg/929/929409.svg" onClick={this.increaseQuantity}/>
+                        <img alt="decrease" className="action-icons" src="https://image.flaticon.com/icons/svg/561/561240.svg" onClick={this.decreaseQuantity}/>
                         <img alt="delete" className="action-icons" src="https://image.flaticon.com/icons/svg/812/812853.svg"/>
                     </div>
                 </div>
